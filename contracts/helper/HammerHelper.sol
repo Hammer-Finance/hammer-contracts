b3d32ab0ba4c36859445337fe510330816db1982
Chi-AnTai <andy.chian.tai@gmail.com>
2020-12-30 14:52:29 +0800

    fix: fix slither warning

--- contracts/helper/HammerHelper.sol
+++ contracts/helper/HammerHelper.sol
@@ -14,26 +14,27 @@ contract HammerHelper {
     using SafeERC20 for IERC20;
     using Address for address;
     using SafeMath for uint256;
-    address public constant weth = address(0xd0A1E359811322d97991E03f863a0C30C2cF029C);
+
+    address public constant WRAP_ETH_ADDRESS = address(0xd0A1E359811322d97991E03f863a0C30C2cF029C);
     
-    function depositEthToBalancer(address balAndWethPoolAddress, uint256 minBptOut) payable public {
+    function depositEthToBalancer(address balAndWethPoolAddress, uint256 minBptOut) payable external {
         require(msg.value > 0, "ERR: No ETH sent");
         uint256 bptAmount = supplyEthToBalancer(balAndWethPoolAddress, msg.value, minBptOut);
-        IERC20(balAndWethPoolAddress).transfer(msg.sender, bptAmount);
+        IERC20(balAndWethPoolAddress).safeTransfer(msg.sender, bptAmount);
     }
     
-    function depositEthToHammer(address balAndWethPoolAddress, address hammerAddress, uint256 minBptOut) payable public {
+    function depositEthToHammer(address balAndWethPoolAddress, address hammerAddress, uint256 minBptOut) payable external {
         require(msg.value > 0, "ERR: No ETH sent");
         uint256 bptAmount = supplyEthToBalancer(balAndWethPoolAddress, msg.value, minBptOut);
         uint256 hbptAmount = supplyBptToHammer(balAndWethPoolAddress, hammerAddress, bptAmount);
-        IERC20(balAndWethPoolAddress).transfer(msg.sender, hbptAmount);
+        IERC20(balAndWethPoolAddress).safeTransfer(msg.sender, hbptAmount);
     }
     
     function supplyEthToBalancer(address balAndWethPoolAddress, uint256 amount, uint256 minBptOut) private returns (uint256) {
-        IWETH(weth).deposit.value(amount)();
-        IERC20(weth).safeApprove(balAndWethPoolAddress, 0);
-        IERC20(weth).safeApprove(balAndWethPoolAddress, amount);
-        uint256 bptAmount = IBPool(balAndWethPoolAddress).joinswapExternAmountIn(weth, amount, minBptOut);
+        IWETH(WRAP_ETH_ADDRESS).deposit.value(amount)();
+        IERC20(WRAP_ETH_ADDRESS).safeApprove(balAndWethPoolAddress, 0);
+        IERC20(WRAP_ETH_ADDRESS).safeApprove(balAndWethPoolAddress,amount);
+        uint256 bptAmount = IBPool(balAndWethPoolAddress).joinswapExternAmountIn(WRAP_ETH_ADDRESS, amount, minBptOut);
         return bptAmount;
     }
     
